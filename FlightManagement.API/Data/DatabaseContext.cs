31efedd98ad383b0b5357f8cf66956c6ff793fc9
Leahu Vlad Iulius <leahu.vlad.iulius@gmail.com>
2022-11-05 19:35:41 +0200

    Initial commit

---
+++ FlightManagement.API/Data/DatabaseContext.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace FlightManagement.API.Data
+{
+    public class DatabaseContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;" +
+            //    "Initial Catalog=[AbsoluteFolderPath]\\SHELTER.MDF;" +
+            //    "Integrated Security=True;Connect Timeout=30;Encrypt=False" +
+            //    "TrustServerCertificate=False;ApplicationIntent=ReadWrite;" +
+            //    "MultiSubnetFailover=False");
+
+            optionsBuilder.UseSqlite("Data Source = FlightManagement.db");
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+        }
+    }
+}
---
+++ FlightManagement.Business/Helpers/ResultOfEntity.cs
@@ -0,0 +1,28 @@
+﻿namespace FlightManagement.Business.Helpers
+{
+    public class Result<TEntity>
+    {
+        public TEntity Entity { get; set; }
+        public string Error { get; private set; }
+        public bool IsScuccess { get; private set; }
+        public bool IsFailure { get; private set; }
+
+        public static Result<TEntity> Success(TEntity entity)
+        {
+            return new Result<TEntity>
+            {
+                Entity = entity,
+                IsScuccess = true
+            };
+        }
+
+        public static Result<TEntity> Failure(string error)
+        {
+            return new Result<TEntity>
+            {
+                Error = error,
+                IsFailure = true
+            };
+        }
+    }
+}
